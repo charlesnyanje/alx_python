f6a15fbc4d4fe8ea88b49d318b8312d7de18dcbe
charlesnyanje <charleswilliam26@gmail.com>
2023-07-22 12:38:50 +0300

    00...99

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,6 +1,4 @@
 #printing a combination of two digits from 00 to 99
-def comb2():
-    for i in range(0, 99):
-        print("{:02d}".format(i), end=", ")
-    print("{:d}".format(i+1))
-comb2()
+for i in range(0, 100):
+    print("{:02d}".format(i), end="")
+    print(", " if i < 99 else "\n", end="")
