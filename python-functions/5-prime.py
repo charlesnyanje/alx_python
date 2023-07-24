1cd7c1fe87603118dff727a1d55df515ab019a6a
charlesnyanje <charleswilliam26@gmail.com>
2023-07-24 12:05:35 +0300

    5. Prime Number Function

---
+++ python-functions/5-prime.py
@@ -0,0 +1,9 @@
+def is_prime(number):
+    """Return True if *number* is prime."""
+    if number <= 1:
+        return False
+    for element in range(2, number):
+        if number % element == 0:
+            return False
+    return True
+is_prime(1)
