230093608d8172d7b103b562037392f81c5c1367
charlesnyanje <charleswilliam26@gmail.com>
2023-07-28 23:12:48 +0300

    0. Import a simple function from a simple file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,4 +2,7 @@ from add_0 import add
 print("0_add is set to: {}".format(__name__))
 a=1
 b=2
-print("{} + {} = {}".format(a, b, add(1, 2)))
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, add(a, b)))
+else:
+    print("I am not a main program")
