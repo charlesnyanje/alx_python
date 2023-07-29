2803aa59df735dae6b32ee6cf7ad81a5ba90d6f5
charlesnyanje <charleswilliam26@gmail.com>
2023-07-29 10:24:59 +0300

    0. Can you C me now?

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,4 +1,2 @@
-#Write a function that removes all characters c and C from a string.
 def no_c(my_string):
     return my_string.translate({ord('c'): None, ord('C'): None})
-print(no_c("Colberton School"))
