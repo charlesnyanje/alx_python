b223d2549645a277a80eb954f7de19a2af529176
charlesnyanje <charleswilliam26@gmail.com>
2023-08-04 08:49:09 +0300

    0. Square with size

--- python-classes/0-main.py
+++ python-classes/0-main.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
-"""import class Square"""
 Square = __import__('0-square').Square
+"""import class Square"""
 
-"""create an instance of Square"""
 my_square = Square(3)
 print(type(my_square))
 print(my_square.__dict__)
+"""create an instance of Square"""
 
-"""handle attribute error"""
 try:
     print(my_square.size)
 except Exception as e:
@@ -17,3 +16,4 @@ try:
     print(my_square.__size)
 except Exception as e:
     print(e)
+"""handle attribute error"""
