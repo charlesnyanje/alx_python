7860f3c69ee5795595502c7c39cdce5b1bf77b32
charlesnyanje <charleswilliam26@gmail.com>
2023-08-04 09:58:18 +0300

    0. Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,18 +1,20 @@
-"""Square class with a private attribute - size.
-    Args:  
-            size (int): size of the square.
-
-    Raises:
-
-            TypeError: if size is not an integer.
+"""This class uses a module 0-main.py to test the class Square.
 """
+
 class Square:
+    """class Square with a private attribute - size.
+     Args:  
+            size (int): size of the square.
+    
+    """
 
     def __init__(self, size=0):
         """_summary_
 
         Args:
             size (int, optional): _description_. Defaults to 0.
+            
+            instance attribute: size
         """
         self.__size = size
 
