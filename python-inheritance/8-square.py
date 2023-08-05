cb28c3e2d09713a0a538900f809467e67a3cb706
charlesnyanje <charleswilliam26@gmail.com>
2023-08-05 18:53:34 +0300

    8. Square #1

---
+++ python-inheritance/8-square.py
@@ -0,0 +1,13 @@
+class Square:
+    def __init__(self, size):
+        self.__size = size
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        return value
+    def area(self):
+        return self.__size * self.__size
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
