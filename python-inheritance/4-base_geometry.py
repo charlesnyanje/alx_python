c27c64ce12cd729c7d2896621138c43313096981
charlesnyanje <charleswilliam26@gmail.com>
2023-08-05 13:39:20 +0300

    4. Improve Geometry

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,5 +1,8 @@
+"""This class uses the 4-main.py module to test the 4-base_geometry.py module.
+"""""
 class BaseGeometry:
-    """ BaseGeometry class """
+    """ BaseGeometry class.
+    """
     def area(self):
         """ Raise an exception """
         raise Exception("area() is not implemented")
