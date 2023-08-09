cc2008bf5f3fab9d4680691d458b4d030c823b07
charlesnyanje <charleswilliam26@gmail.com>
2023-08-09 16:46:41 +0300

    0. Base class

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,14 +1,23 @@
-"""This class uses the __nb_objects attribute to assign an id to each instance"""
+"""This class uses the __nb_objects attribute to assign an id to each instance>
+"""
+
+
 class Base:
-    """Base class"""
-    def __init__(self, id=None,__nb_objects=0):
-        """method init"""""
+    """Base class.
+    """
+
+    def __init__(self, id=None, __nb_objects=0):
+        """method init.
+        """
         if id != None:
             self.id = id
         else:
             __nb_objects += 1
             self.id = __nb_objects
-"""Instance of Base class"""
+
+
+"""Instance of Base class.
+"""
 
 b1 = Base()
 b2 = Base()
