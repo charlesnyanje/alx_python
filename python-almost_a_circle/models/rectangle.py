08171d327ae86c39524e720d138a0f0c1b62b9ca
charlesnyanje <charleswilliam26@gmail.com>
2023-08-09 22:02:06 +0300

    5. __str__

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -92,5 +92,5 @@ class Rectangle(Base):
             
     def __str__(self):
         """str method."""
-        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height})"
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
     
