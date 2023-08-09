8d089dc956359ecb233b28e91f888de2eddbb09b
charlesnyanje <charleswilliam26@gmail.com>
2023-08-09 21:30:13 +0300

    2. Validate attributes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -11,16 +11,32 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """init method."""
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     """Getters and setters."""
+
     @property
     def width(self):
         """width getter."""
         return self.__width
+
+    @property
+    def height(self):
+        """height getter."""
+        return self.__height
+
+    @property
+    def x(self):
+        """x getter."""
+        return self.__x
+
+    @property
+    def y(self):
+        """y getter."""
+        return self.__y
 
     @width.setter
     def width(self, value):
@@ -32,11 +48,6 @@ class Rectangle(Base):
         else:
             self.__width = value
 
-    @property
-    def height(self):
-        """height getter."""
-        return self.__height
-
     @height.setter
     def height(self, value):
         """height setter."""
@@ -47,11 +58,6 @@ class Rectangle(Base):
         else:
             self.__height = value
 
-    @property
-    def x(self):
-        """x getter."""
-        return self.__x
-
     @x.setter
     def x(self, value):
         """x setter."""
@@ -61,12 +67,7 @@ class Rectangle(Base):
             raise ValueError("x must be >= 0")
         else:
             self.__x = value
-            
-    @property
-    def y(self):
-        """y getter."""
-        return self.__y
-    
+
     @y.setter
     def y(self, value):
         """y setter."""
