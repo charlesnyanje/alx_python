b6b82b70ee244abf5dbcf7f1daa845cefcd24f54
charlesnyanje <charleswilliam26@gmail.com>
2023-08-10 09:16:36 +0300

    9. And now, the Square!

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,13 +1,13 @@
-"""Import the module Rectangle.
-"""
 from models.rectangle import Rectangle
 """class square inherited from Rectangle.
 """
 class Square(Rectangle):
     """Square class.
     """
+    """init method.
+    """
     def __init__(self, size, x=0, y=0, id=None):
-        """init method.
+        """attributes of square class.
         """
         super().__init__(size, size, x, y, id)
         self.size = size
