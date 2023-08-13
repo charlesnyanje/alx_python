70b377d3bbe7584ad3ba0dfcf91e51420328ecf2
charlesnyanje <charleswilliam26@gmail.com>
2023-08-13 15:26:52 +0300

    5. My GitHub!

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -1,9 +1,4 @@
-"""Write a Python script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id
-
-You must use Basic Authentication with a personal access token as password to access to your information (only read:user permission is needed)
-The first argument will be your username
-The second argument will be your password (in your case, a personal access token as password)
-You must use the package requests and sys."""
+"""Python script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id."""
 
 import requests
 import sys
