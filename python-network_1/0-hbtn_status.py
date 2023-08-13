97514b4c7b915059f5bd8923fbe3bbfeb56bf75f
charlesnyanje <charleswilliam26@gmail.com>
2023-08-13 14:41:10 +0300

    0. What's my status? #1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -11,8 +11,7 @@ url = "https://alu-intranet.hbtn.io/status"
 req = Request("GET", url)
 
 if __name__ == "__main__":
-    """body response.
-    """
-    response = requests.get(url)
-    print("Body response:\t")
-    
+    r = requests.get(url)
+    print("Body response:")
+    print("\t- type: ", type(r.text))
+    print("\t- content: ", r.text)
