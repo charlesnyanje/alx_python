647f18cec33ff49a40af0e40d6648c4149da91b5
charlesnyanje <charleswilliam26@gmail.com>
2023-08-13 14:44:24 +0300

    0. What's my status? #1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,7 +1,5 @@
-"""Write a Python script that fetches https://alu-intranet.hbtn.io/status
-
-You must use the package requests
-You are not allow to import packages other than requests."""
+""" Python script that fetches https://alu-intranet.hbtn.io/status using requests package.
+"""
 
 import requests
 from requests import Request
@@ -13,5 +11,5 @@ req = Request("GET", url)
 if __name__ == "__main__":
     r = requests.get(url)
     print("Body response:")
-    print("\t- type: ", type(r.text))
-    print("\t- content: ", r.text)
+    print("\t- type:", type(r.text))
+    print("\t- content:", r.text)
