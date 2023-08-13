c145712a69a8517b5c450242cd724d9b26cf4a78
charlesnyanje <charleswilliam26@gmail.com>
2023-08-13 10:13:24 +0300

    0. What's my status? #1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,13 @@
-# Description: Python script that fetches https://intranet.hbtn.io/status
 #!/usr/bin/python3
-import urllib.request
+""" Python script that fetches https://intranet.hbtn.io/status.
+"""
+import urllib.request as requests
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    with requests.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
         print("\t- utf8 content: {}".format(html.decode('utf-8')))
-        
-        
-        
+
