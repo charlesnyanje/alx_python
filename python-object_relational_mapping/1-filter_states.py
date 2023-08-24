05397a6d7a0199492d1725034b4450fd3aff9e78
charlesnyanje <charleswilliam26@gmail.com>
2023-08-24 14:48:20 +0300

    1. Filter states

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,38 @@
+"""Write a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa:
+
+Your script should take 3 arguments: mysql username, mysql password and database name (no argument validation needed)
+You must use the module MySQLdb (import MySQLdb)
+Your script should connect to a MySQL server running on localhost at port 3306
+Results must be sorted in ascending order by states.id
+Results must be displayed as they are in the example below
+Your code should not be executed when imported."""
+import MySQLdb
+import sys
+
+
+def filter_states(username, password, database):
+    try:
+
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        )
+        cursor = db.cursor()
+        cursor.execute(
+            "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+        cursor.close()
+        db.close()
+
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    filter_states(sys.argv[1], sys.argv[2], sys.argv[3])
