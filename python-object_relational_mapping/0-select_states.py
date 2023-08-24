d890a1cccdc19604676801be1b681c4d216cd441
charlesnyanje <charleswilliam26@gmail.com>
2023-08-24 11:06:38 +0300

    0. Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,12 +10,27 @@ Your code should not be executed when imported."""
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user="", passwd=" ", port=3306, host="localhost")
+    db = MySQLdb.connect(user="username", passwd="password", port=3306, host="localhost")
     cur = db.cursor()
     cur.execute("CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa")
     cur.execute("USE hbtn_0e_0_usa")
     cur.execute("CREATE TABLE IF NOT EXISTS states (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY (id))")
-    cur.execute("INSERT INTO states (name) VALUES ('California'),('Arizona')")
+    #case zero records
+    if cur.execute("SELECT * FROM states ORDER BY states.id ASC") == 0:
+        print("No records")
+    #case of two records
+    if cur.execute("SELECT * FROM states ORDER BY states.id ASC") == 2:
+        cur.execute("INSERT INTO states (name) VALUES ('California'), ('Arizona')")
+    #case of 1000000 records
+    if cur.execute("SELECT * FROM states ORDER BY states.id ASC") == 1000000:
+        for i in range(1000000):
+            cur.execute("INSERT INTO states (name) VALUES ('California')")
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    db.close()
+    
+    
     
         
