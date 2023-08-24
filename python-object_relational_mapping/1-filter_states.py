cbbad896e5a647164a4eeaa59874179d062c9388
charlesnyanje <charleswilliam26@gmail.com>
2023-08-24 16:46:27 +0300

    1. Filter states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -23,7 +23,6 @@ def list_states(username, password, database):
 
         """Execute the SQL query to fetch states.
         """
-        #line < 79 characters
         cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id ASC")        
         """Fetch all rows.
         """
