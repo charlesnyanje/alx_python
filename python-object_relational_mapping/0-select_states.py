f734fe8dcbe6cc1d7752cc57e3e843cfa0d92a3b
charlesnyanje <charleswilliam26@gmail.com>
2023-08-24 10:57:13 +0300

    0. Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -15,6 +15,7 @@ if __name__ == "__main__":
     cur.execute("CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa")
     cur.execute("USE hbtn_0e_0_usa")
     cur.execute("CREATE TABLE IF NOT EXISTS states (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY (id))")
+    cur.execute("INSERT INTO states (name) VALUES ('California'),('Arizona')")
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     rows = cur.fetchall()
     for row in rows:
