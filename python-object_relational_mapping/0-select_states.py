44b1e4ed2ec89092ee7a37db882ef06418b11949
charlesnyanje <charleswilliam26@gmail.com>
2023-08-24 14:17:57 +0300

    0. Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,14 @@
 """Write a script that lists all states from the database hbtn_0e_0_usa:
-
 Your script should take 3 arguments: mysql username, mysql password and database name (no argument validation needed)
 You must use the module MySQLdb (import MySQLdb)
 Your script should connect to a MySQL server running on localhost at port 3306
 Results must be sorted in ascending order by states.id
 Results must be displayed as they are in the example below
-Your code should not be executed when imported."""
+Your code should not be executed when imported.
+"""
 
 import MySQLdb
 import sys
-
 
 def list_states(username, password, database):
     try:
@@ -48,7 +47,6 @@ def list_states(username, password, database):
     except MySQLdb.Error as e:
         print("MySQL Error:", e)
         sys.exit(1)
-
 
 if __name__ == "__main__":
     list_states(sys.argv[1], sys.argv[2], sys.argv[3])
