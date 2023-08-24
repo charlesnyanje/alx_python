5564c41284a5651e263ee9982e5131072fb60240
charlesnyanje <charleswilliam26@gmail.com>
2023-08-24 10:16:50 +0300

    0. Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,14 +8,17 @@ Results must be displayed as they are in the example below
 Your code should not be executed when imported."""
 
 import MySQLdb
-import sys
-if __name__ == "__main__":
-    db = MySQLdb.connect(user="sys.argv[1]", passwd="sys.argv[2]",
-                         db="sys.argv[3]", port=3306, host="localhost")
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+
+db = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="Charles@26")
+
+cur = db.cursor()
+
+cur.execute("CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa")
+cur.execute("USE hbtn_0e_0_usa")
+cur.execute("CREATE TABLE IF NOT EXISTS states (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(256), PRIMARY KEY (id))")
+cur.execute("INSERT INTO states (name) VALUES ('California'), ('Arizona'), ('Texas'), ('Nevada'), ('Utah'), ('Colorado'), ('New Mexico'), ('Wyoming'), ('Montana'), ('Idaho'), ('Washington'), ('Oregon'), ('North Dakota'), ('South Dakota'), ('Nebraska'), ('Kansas'), ('Oklahoma'), ('Minnesota'), ('Iowa'), ('Missouri'), ('Arkansas'), ('Louisiana'), ('Wisconsin'), ('Illinois'), ('Mississippi'), ('Michigan'), ('Indiana'), ('Kentucky'), ('Tennessee'), ('Alabama'), ('Ohio'), ('West Virginia'), ('Virginia'), ('North Carolina'), ('South Carolina'), ('Georgia'), ('Florida'), ('Pennsylvania'), ('New York'), ('Vermont'), ('New Hampshire'), ('Maine'), ('Massachusetts'), ('Rhode Island'), ('Connecticut'), ('New Jersey'), ('Delaware'), ('Maryland'), ('Alaska'), ('Hawaii')")
+cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+for row in cur.fetchall():
+    print(row)
+cur.close()
+db.close()
