00e8d43a4527136dcd94f76e8584bc905a34339a
charlesnyanje <charleswilliam26@gmail.com>
2023-09-02 10:16:33 +0300

    9. Contains `a`

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,1 +1,36 @@
- 
+"""Write a script that lists all State objects that contain the letter a from the database hbtn_0e_6_usa
+
+Your script should take 3 arguments: mysql username, mysql password and database name
+You must use the module SQLAlchemy
+You must import State and Base from model_state - from model_state import Base, State
+Your script should connect to a MySQL server running on localhost at port 3306
+Results must be sorted in ascending order by states.id
+The results must be displayed as they are in the example below
+Your code should not be executed when imported."""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State  # Import the State and Base classes from model_state
+
+def list_states_with_a(username, password, database_name):
+    # Define the database connection URL
+    db_url = f"mysql://{username}:{password}@localhost:3306/{database_name}"
+
+    # Create the SQLAlchemy engine
+    engine = create_engine(db_url)
+
+    # Create a session to interact with the database
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query the database for State objects containing the letter 'a' in their name
+    states_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+
+    # Display the results
+    for state in states_with_a:
+        print(f"{state.id}: {state.name}")
+
+if __name__ == "__main__":
+    username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    list_states_with_a(username, password, database_name)
