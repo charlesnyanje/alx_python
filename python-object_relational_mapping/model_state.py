84fed08dc44bee552d6fa6bf854c54fe39a0950d
charlesnyanje <charleswilliam26@gmail.com>
2023-09-02 09:50:00 +0300

    6. First state model

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,37 @@
+"""Write a python file that contains the class definition of a State and an instance Base = declarative_base():
+
+State class:
+inherits from Base Tips
+links to the MySQL table states
+class attribute id that represents a column of an auto-generated, unique integer, can’t be null and is a primary key
+class attribute name that represents a column of a string with maximum 128 characters and can’t be null
+You must use the module SQLAlchemy
+Your script should connect to a MySQL server running on localhost at port 3306
+WARNING: all classes who inherit from Base must be imported before calling Base.metadata.create_all(engine)"""
+
+import sqlalchemy
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# database connection url
+db_url = "mysql://username:password@localhost:3306/your_database"
+
+engine = create_engine(db_url)
+
+Base = declarative_base()
+
+
+class State(Base):
+
+    __tablename__ = "states"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+    if __name__ == "__main__":
+        Base.metadata.create_all(engine)
+
+    # Optionally, you can create a session for database operations
+    Session = sessionmaker(bind=engine)
+    session = Session()
