b99e0a6feafa1633e1fe8f5c229173861678bd11
charlesnyanje <charleswilliam26@gmail.com>
2023-09-06 10:50:10 +0300

    2. C is fun!

--- python-web_framework/2-c_route.py
+++ python-web_framework/2-c_route.py
@@ -20,7 +20,7 @@ def hbnb():
 
 @app.route("/c/<text>", strict_slashes = False)
 def c(text):
-    return f"C {text}"
+    return f"C {text}".replace("_"," ")
 
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0",port="5000")
