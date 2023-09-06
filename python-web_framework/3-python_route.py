696cd4987a098ff8095af95fb4119f64b5fd06a3
charlesnyanje <charleswilliam26@gmail.com>
2023-09-06 11:35:48 +0300

    3. Python is cool!

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -22,7 +22,7 @@ def hbnb():
 def c(text):
     return f"C {text}".replace("_"," ")
 
-@app.route("/python/<text>",defaults = {'text':''}, strict_slashes = False)
+@app.route("/python/<text>", strict_slashes = False)
 def python(text):
     return f"Python {text}".replace("_"," ")
 
