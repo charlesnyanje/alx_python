6e2ee4e205568b3c1e4f3b6a00fbe1d5d75ecf5e
charlesnyanje <charleswilliam26@gmail.com>
2023-09-06 11:03:40 +0300

    3. Python is cool!

---
+++ python-web_framework/3-python_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application.
+"""
+
+
+from flask import Flask
+"""Initialize app.
+"""
+app = Flask(__name__)
+"""routes definition.
+"""
+
+@app.route("/", strict_slashes = False)
+def home():
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes = False)
+def hbnb():
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes = False)
+def c(text):
+    return f"C {text}".replace("_"," ")
+
+@app.route("/python/<text>", strict_slashes = False)
+def python(text):
+    return f"Python {text}".replace("_"," ")
+
+
+if __name__ == "__main__":
+    app.run(debug=True,host="0.0.0.0",port="5000")
